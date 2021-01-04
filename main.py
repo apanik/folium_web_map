7a2463cc038853449b7ed9261b57dc4a9caa9953
apanik <ap.anik71@gmail.com>
2021-01-04 15:05:32 +0600

    tooltip added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location=[23.777176,90.399452], tiles="Mapbox Bright")
+map = folium.Map(location=[23.777176,90.399452], zoom_start=9)
+tooltip = 'Click me!'
+
+folium.Marker([45.3288, -121.6625], popup='<i>Mt. Hood Meadows</i>', tooltip=tooltip).add_to(m)
+folium.Marker([45.3311, -121.7113], popup='<b>Timberline Lodge</b>', tooltip=tooltip).add_to(m)
 map.save("Map.html")
