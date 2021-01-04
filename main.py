953fed93323d42d2ca03714b6e6c7e29ad46dc4a
apanik <ap.anik71@gmail.com>
2021-01-04 14:51:10 +0600

    mapbox to Mapbox issue fixed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import folium
 
-map = folium.Map(location=[23.777176,90.399452], tiles = "mapbox bright")
+map = folium.Map(location=[23.777176,90.399452], tiles = "Mapbox bright")
 map.save("Map.html")
